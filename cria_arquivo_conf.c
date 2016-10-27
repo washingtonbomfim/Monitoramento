04a62b11ebef05a0435c02e2c6f8032219d6dbb1
washington <washington1bomfim@gmail.com>
2016-10-27 14:27:13 -0200

    swap arquivo

--- cria_arquivo_conf.c
+++ cria_arquivo_conf.c
@@ -18,6 +18,24 @@ void verifica_arquivos(){
         fclose(arquivo);
     }
     
+    arquivo = fopen(ARQ_IP_DOWN,"r");
+       
+    if(!arquivo){
+        arquivo = fopen(ARQ_IP_DOWN,"wb");
+        fclose(arquivo);
+    }else{
+        fclose(arquivo);
+    }
+    
+    arquivo = fopen(ARQ_IP_DOWN_TMP,"r");
+       
+    if(!arquivo){
+        arquivo = fopen(ARQ_IP_DOWN_TMP,"wb");
+        fclose(arquivo);
+    }else{
+        fclose(arquivo);
+    }
+    
     arquivo = fopen(ARQ_LOG,"r");
     
     if(!arquivo){
@@ -35,7 +53,6 @@ void verifica_arquivos(){
     }else{
         fclose(arquivo);
     }
-    
 }
 
 void adiciona_telefone(char *telefone){
@@ -89,6 +106,7 @@ void exibe_ip_configurado(){
     }
     fclose(arquivo);
 }
+
 void exibe_numero_configurado(){
     
     FILE *arquivo;
@@ -124,6 +142,101 @@ void gera_log(char *relatorio){
     arquivo = fopen(ARQ_LOG,"a");
     fprintf(arquivo,"Horario : %s Mensagem = %s\n",hora,relatorio);
     fclose(arquivo);
+}
+
+void adiciona_ips_down(char *ip){
+
+    FILE *arquivo;
+    
+    arquivo = fopen(ARQ_IP_DOWN,"a");
+    fprintf(arquivo,"%s;01\n",ip);
+    fclose(arquivo);
+    
+}
+
+int remove_ip_down(char *ip){
+    FILE *arquivo_antigo;
+    FILE *arquivo_novo;
+    int line = 0;
+    int linha = 1;
+    int i;
+    char textoArquivo[100];
+    char *output;
+
+    rewind(arquivo_antigo);
+    for(i = 0; !feof(arquivo_antigo);i++){ 
+        memset(textoArquivo, '{FONTE}', 100); 
+        fgets(textoArquivo, 101, arquivo_antigo);
+        if(linha == line){
+            linha = linha + 1;
+            continue;
+        }
+        linha = linha + 1;
+        fputs(textoArquivo, arquivo_novo);
+    }
+
+    free(output);
+    fclose(arquivo_antigo);
+    fclose(arquivo_novo);
+}
+
+int busca_ip_down(char *ip){
+    
+    FILE *arquivo_antigo;
+    FILE *arquivo_novo;
+    int i = 0, j=0, retorno = 0;
+    char buffer[500],qtd[3];
+    
+    memset(&qtd, 0, sizeof(qtd));
+    memset(&buffer, 0, sizeof(buffer));
+    arquivo_antigo = fopen(ARQ_IP_DOWN,"r");
+    //arquivo_novo   = fopen(ARQ_IP_DOWN_TMP,"a");
+    rewind(arquivo_antigo);
+    
+    while(1){    
+        buffer[i]=fgetc(arquivo_antigo);
+        
+        if(buffer[i] == EOF){
+            break;
+        }
+        
+        if(buffer[i] == ';'){
+            buffer[i] = '\0';
+            printf("b %s - %s\n",buffer,ip);
+            if(strcmp(buffer,ip)==0){
+                retorno++;
+                //ponto_corrente = fseek(arquivo_antigo,0,SEEK_CUR);
+                buffer[i] = ';';
+                for(j=0;j<2;j++){
+                    qtd[j]=fgetc(arquivo_antigo);
+                }
+                qtd[j++] = '\0';
+                if(strcmp(qtd,"20")==0){
+                    retorno++;
+                    remove_ip_down(ip);
+                    printf("Deu certo\n");
+                    
+                }else{
+                    buffer[i++]=';';
+                    buffer[i++]=qtd[0];
+                    buffer[i++]=qtd[1];
+                    buffer[i++]=fgetc(arquivo_antigo);
+                }
+                memset(&buffer, 0, sizeof(buffer));
+                i=-1;
+            }else{
+                printf("degub-else\n");
+                printf("%c",buffer[i]);
+                i++;
+            }
+        }
+        i++;
+    }
+    printf("\n");
+    printf("end\n");
+    fclose(arquivo_antigo);
+    return retorno;
+    //fclose(arquivo_novo);
 }
 
 int preprara_envio_sms(char *relatorio){
